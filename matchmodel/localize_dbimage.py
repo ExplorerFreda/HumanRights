4951f325c2efd6d277fa331bf6e05226f0a5ac21
ExplorerFreda <hyshi@pku.edu.cn>
2016-11-28 07:41:53 -0500

    finish localization procedure

--- matchmodel/localize_dbimage.py
+++ matchmodel/localize_dbimage.py
@@ -1,4 +1,5 @@
 image_num = 10000
+delta = 3 # threshold for location matching
 caffe_root = '/home/hyshi/caffe/'
 import sys
 sys.path.insert(0, caffe_root + 'python')
@@ -7,18 +8,22 @@ import numpy as np
 import json
 from numpy import matlib as mt
 
+###
+# Result: 
+#   fine-tuned: 4231
+###
+
 caffe.set_mode_gpu()
 db_filename = '/mnt/sda/backup/match/database.json'
 dist_filename = '/mnt/sda/backup/match/dist.json'
 vectors = []
 
-fout = open(dist_filename, 'w')
 
 def CalcDist(x):
   return np.sum(np.array(x)**2)
 
-
-
+'''
+fout = open(dist_filename, 'w')
 for line in open(db_filename):
   vector = json.loads(line)
   vectors.append(vector)
@@ -30,3 +35,24 @@ for idx in range(image_num):
   print idx
 
 fout.close()
+'''
+
+def location(idx):
+  return ((idx/4)/int(image_num**0.5/2), (idx/4)%int(image_num**0.5/2))
+
+correct_match = 0
+
+fin = open(dist_filename)
+for idx in range(image_num):
+  line = fin.readline()
+  dist_info = json.loads(line)
+  dist_info = [(dist_info[i], i) for i in range(image_num)]
+  dist_info = sorted(dist_info, key=lambda x:x[0])
+  loc_idx = location(idx)
+  for item in dist_info[1:6]:
+    loc_idy = location(item[1])
+    if max(abs(loc_idx[0] - loc_idy[0]),abs(loc_idx[1] - loc_idy[1])) < delta:
+      correct_match += 1
+  print correct_match, idx + 1, float(correct_match) / (idx + 1)
+print correct_match
+fin.close()
