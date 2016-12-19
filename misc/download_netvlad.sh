d7ee71c9a469d1181617449f80c3cd5fb9f79fde
ExplorerFreda <hyshi@pku.edu.cn>
2016-12-19 08:37:08 -0500

    add download tokyo dataset

---
+++ misc/download_netvlad.sh
@@ -0,0 +1,17 @@
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AABo-ABtV1q9kFnyikSyS3RRa/03814.tar -O /mnt/sda/backup/NetVLad/0.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AABqAtg8BnsHMGfqWcIzzKKua/03815.tar -O /mnt/sda/backup/NetVLad/1.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AACVbzasVBCsWkk0u1jc-5pda/03816.tar -O /mnt/sda/backup/NetVLad/2.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AAAw5vQ5Oki2gsumI8T2y9_xa/03817.tar -O /mnt/sda/backup/NetVLad/3.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AAAcWFLgvCJkI8AkDLyFVUUBa/03818.tar -O /mnt/sda/backup/NetVLad/4.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AABd_X4xupMmZG8a_B9TNVa7a/03819.tar -O /mnt/sda/backup/NetVLad/5.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AAAqPQKCkBqONt3Xuwksl9B-a/03820.tar -O /mnt/sda/backup/NetVLad/6.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AADyJSWZx0XlXNzN54ofRYxya/03821.tar -O /mnt/sda/backup/NetVLad/7.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AADmEmjqyNRgZ-BGsYT3j5cGa/03822.tar -O /mnt/sda/backup/NetVLad/8.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AAA8IaOA2fSqYLHq2w4aFOgpa/03823.tar -O /mnt/sda/backup/NetVLad/9.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AACFpNm3hsiuNUMcPprLyQZZa/03824.tar -O /mnt/sda/backup/NetVLad/10.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AACrfSmiSwirnZFGkuaX9Jqga/03825.tar -O /mnt/sda/backup/NetVLad/11.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AAAblQNeeDilYR3ZejpiXeSDa/03826.tar -O /mnt/sda/backup/NetVLad/12.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AACPl-YJcjdZECtQ-eq7Jbgsa/03827.tar -O /mnt/sda/backup/NetVLad/13.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AABFyvQUJtLay6jj8vbkoylaa/03828.tar -O /mnt/sda/backup/NetVLad/14.tar
+wget https://www.dropbox.com/sh/0l04qchbc73kigr/AACPiIFR545CiPRMlufg76Uoa/03829.tar -O /mnt/sda/backup/NetVLad/15.tar
+
