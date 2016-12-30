7ce6470b53db6ce6085e9d30f46cbcaead848220
ExplorerFreda <hyshi@pku.edu.cn>
2016-12-30 08:17:24 -0500

    add generate wdatabase logic

--- netvlad/generate_database.py
+++ netvlad/generate_database.py
@@ -4,64 +4,25 @@ sys.path.insert(0, caffe_root + 'python')
 import caffe
 import numpy as np
 import json
-
+import os
 
 
 ######### Config NetVLad Finetuned #############
 model  = 'deploy.prototxt'
-weights = '/mnt/sda/backup/match/netvlad_model_iter_20000.caffemodel'
+weights = '/mnt/sda/backup/match/netvlad-7_model_iter_45000.caffemodel'
 db_filename = '/mnt/sda/backup/match/netvlad_vector.json'
+pyramid_db_filename = '/mnt/sda/backup/match/netvlad_pyramid_vector.json'
 ######### Config NetVLad Finetuned #############
+
 
 '''
 ######### Config VGG-CNN-M Model #############
 model  = 'deploy.prototxt'
 weights = '/mnt/sda/backup/45degree/model/VGG_CNN_M.caffemodel'
 db_filename = '/mnt/sda/backup/match/netvlad_vgg-cnn-m_vector.json'
+pyramid_db_filename = '/mnt/sda/backup/match/netvlad_vgg-cnn-m_pyramid_vector.json'
 ######### Config VGG-CNN-M Model #############
 '''
-
-
-def GetFileList(d):
-	file_list = []
-	if os.path.isfile(d):
-		file_list.append(d)
-	elif os.path.isdir(d):
-		for f in os.listdir(d):
-			new_dir = os.path.join(d,f)
-			file_list.extend(GetFileList(new_dir))
-	return file_list
-
-
-def GenerateDatabaseSingle(db_filename, file_list):
-	fout = open(db_filename, 'w')
-	for idx, filename in enumerate(file_list):
-		image = caffe.io.load_image(filename)
-		net.blobs['data'].data[...] = transformer.preprocess('data', image)
-		out = net.forward()
-		vector = out['fc7'].copy()
-		fout.write(json.dumps(vector.tolist()[0])+'\n')
-		print 'Single Database', idx
-	fout.close()
-
-
-def GenerateDataBasePyramid(db_filename, file_list):
-	fout = open(db_filename, 'w')
-	for idx, filename in enumerate(file_list):
-		image = caffe.io.load_image(filename)
-		print type(image)
-		vector = list()
-		for subimage in subimages:
-			net.blobs['data'].data[...] = transformer.preprocess('data', subimage)
-			out = net.forward()
-			vector.append(out['fc7'].copy().tolist()[0])
-		fout.write(json.dumps(vector)+'\n')
-		print 'Pyramid Database', idx, len(vector)
-	fout.close()
-
-
-
-file_list = GetFileList('/mnt/sda/backup/NetVLad/')
 
 caffe.set_mode_gpu()
 net = caffe.Net(model, weights, caffe.TEST)
@@ -72,5 +33,63 @@ transformer.set_raw_scale('data', 255)
 transformer.set_transpose('data', (2, 0, 1))
 transformer.set_mean('data', np.load('/mnt/sda/backup/45degree/VGG_mean.npy'))
 
-GenerateDatabaseSingle(db_filename, file_list)
 
+
+def GetImageFileList(d):
+	file_list = []
+	if os.path.isfile(d) and (d[-3:] == 'png' or d[-3:] == 'jpg'):
+		file_list.append(d)
+	elif os.path.isdir(d):
+		for f in os.listdir(d):
+			new_dir = os.path.join(d,f)
+			file_list.extend(GetImageFileList(new_dir))
+	return file_list
+
+
+def GenerateDatabaseSingle(db_filename, file_list):
+	fout = open(db_filename, 'w')
+	for idx, filename in enumerate(file_list):
+		image = caffe.io.load_image(filename)
+		net.blobs['data'].data[...] = transformer.preprocess('data', image)
+		out = net.forward()
+		vector = out['fc7'].copy()
+
+		fout.write(json.dumps(vector.tolist()[0])+'\n')
+		print 'Single Database', idx
+	fout.close()
+
+
+def GenerateDatabasePyramid(db_filename, file_list):
+	fout = open(db_filename, 'w')
+	for idx, filename in enumerate(file_list):
+		image = caffe.io.load_image(filename)
+		vector = list()
+		subimages = list()
+		# generate subimages
+		for l in range(1,4):
+			length_x = len(image) / l
+			length_y = len(image[0]) / l
+			for i in range(0,len(image)-3,length_x):
+				for j in range(0,len(image[0])-3,length_y):
+					subimages.append(image[i:i+length_x,j:j+length_y].copy())
+		for subimage in subimages:
+			net.blobs['data'].data[...] = transformer.preprocess('data', subimage)
+			out = net.forward()
+			vector.append(out['fc7'].copy().tolist()[0])
+		fout.write(json.dumps(vector)+'\n')
+		print 'Pyramid Database', idx, len(vector)
+	fout.close()
+
+
+def OutputFileList(filelist, filename):
+	fout = open(filename, 'w')
+	for item in filelist:
+		fout.write(item + '\n')
+	fout.close()
+
+
+file_list = GetImageFileList('/mnt/sda/backup/NetVLad/')
+print 'File List Size:', len(file_list)
+#GenerateDatabasePyramid(pyramid_db_filename, file_list)
+GenerateDatabaseSingle(db_filename, file_list)
+OutputFileList(file_list, '/mnt/sda/backup/NetVLad/filelist.txt')
